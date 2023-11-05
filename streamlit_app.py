b50f7b1e3cd323b774f35c6619bca0183624087e
myevilpumpkin <dima.tomin2010@yandex.ru>
2023-11-05 04:20:17 +0300

    Web apps generalized

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,15 +3,15 @@ import sys
 import streamlit as st
 from streamlit.web import cli as stcli
 
-from article_processing import define_genre, summarize
+from article_processing import modules
 
 
 def main():
     st.title("Article Processing")
-    article = st.text_area("Статья", placeholder="Введите статью...")
-    if article != "":
-        st.write(f"Жанр — {define_genre(article)}")
-        st.write(f"Содержание — {summarize(article)}")
+    article_text = st.text_area("Статья", placeholder="Введите статью...")
+    if article_text != "":
+        for module_name, module in modules.items():
+            st.write(f"{module.get('result_title')} — {module.get('function')(article_text)}")
         st.write("Обработка статьи завершена!")
     else:
         st.write("После ввода статьи здесь отобразятся результаты ее обработки")
