a48b7c0ee0c334439a5df67fd03f3f8e241980d3
myevilpumpkin <dima.tomin2010@yandex.ru>
2023-11-24 19:57:11 +0300

    Updated summarization model

--- article_processing/summarization.py
+++ article_processing/summarization.py
@@ -1,30 +1,8 @@
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import pipeline
 
-model_name = "csebuetnlp/mT5_multilingual_XLSum"
-tokenizer = AutoTokenizer.from_pretrained(model_name, legacy=False)
-model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+summarizer = pipeline('summarization', model='d0rj/rut5-base-summ')
 
 
 def execute(text: str) -> str:
-    input_ids = tokenizer(
-        [text],
-        return_tensors="pt",
-        padding="max_length",
-        truncation=True,
-        max_length=512
-    )["input_ids"]
-
-    output_ids = model.generate(
-        input_ids=input_ids,
-        max_length=84,
-        no_repeat_ngram_size=2,
-        num_beams=4
-    )[0]
-
-    summary = tokenizer.decode(
-        output_ids,
-        skip_special_tokens=True,
-        clean_up_tokenization_spaces=False
-    )
-
-    return summary
+    summarization_result = summarizer(text)
+    return summarization_result[0]['summary_text']
