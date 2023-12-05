1c9fe47ce76e60c88ab011c840402d73410c0984
myevilpumpkin <dima.tomin2010@yandex.ru>
2023-12-06 02:11:49 +0300

    Added module selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,15 +6,21 @@ from streamlit.web import cli as stcli
 from article_processing import modules
 
 
+@st.cache_resource
+def module_titles():
+    return dict([(modules[module]["title"], modules[module]) for module in modules])
+
+
 def main():
+    modules_by_titles = module_titles()
     st.title("Article Processing")
-    article_text = st.text_area("Статья", placeholder="Введите статью...")
-    if article_text != "":
-        for module_name, module in modules.items():
-            st.write(f"{module.get('result_title')} — {module.get('function')(article_text)}")
-        st.write("Обработка статьи завершена!")
+    article_text = st.text_area(label="Статья", placeholder="Введите статью...")
+    module_title = st.selectbox(label="Модуль", placeholder="Выберите модуль...", options=modules_by_titles, index=None)
+    if article_text != "" and module_title is not None:
+        module = modules_by_titles[module_title]
+        st.write(f"{module['result_title']} — {module['function'](article_text)}")
     else:
-        st.write("После ввода статьи здесь отобразятся результаты ее обработки")
+        st.write("После ввода статьи и выбора модуля здесь отобразятся результаты ее обработки")
 
 
 if __name__ == "__main__":
