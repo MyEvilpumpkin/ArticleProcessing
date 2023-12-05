aca04e898cbd4ac50c1a642e2f427497ac168e13
myevilpumpkin <dima.tomin2010@yandex.ru>
2023-12-05 19:42:21 +0300

    Added explicit file encoding for testing on windows

--- libs/file_reader.py
+++ libs/file_reader.py
@@ -1,6 +1,6 @@
 
 def read_file(file_path):
     file_name = file_path.split('/')[-1].split('_')[0]
-    with open(file_path, 'r') as file:
+    with open(file_path, 'r', encoding="utf-8") as file:
         file_content = file.read()
     return file_name, file_content
