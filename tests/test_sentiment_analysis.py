bd89de4099b16752919214f74d0f509be4cc2953
Dmitry <dmitry.filimonov@hotmail.com>
2023-12-18 03:21:50 +0300

    Sentiment analysis tests
    Implemented unit tests and added additional test articles

---
+++ tests/test_sentiment_analysis.py
@@ -0,0 +1,19 @@
+from article_processing import analyze_sentiment
+from libs.file_reader import read_file
+
+def execute_test(file_path, expected_sentiment):
+    _, article_text = read_file(file_path)
+    sentiment = analyze_sentiment(article_text)
+    assert sentiment == expected_sentiment, f'Expected {expected_sentiment}, but got {sentiment}'
+
+
+def test_positive_sentiment():
+    execute_test('test_articles/положительная_1.txt', 'положительная')
+
+
+def test_negative_sentiment():
+    execute_test('test_articles/негативная_1.txt', 'негативная')
+
+
+def test_neutral_sentiment():
+    execute_test('test_articles/нейтральная_1.txt', 'нейтральная')
