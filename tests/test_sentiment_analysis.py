54ac5449e775d500979a891317ac4e5504662cb5
myevilpumpkin <dima.tomin2010@yandex.ru>
2024-05-15 15:28:17 +0300

    Refactor tests

--- tests/test_sentiment_analysis.py
+++ tests/test_sentiment_analysis.py
@@ -1,21 +1,12 @@
+import pytest
+
 from modules import analyze_sentiment
 from libs.file_reader import read_file
-from tests.articles import positive_1, negative_1, neutral_1
+from tests.articles import sentiment_analysis_articles as articles
 
 
-def execute_test(file_path):
+@pytest.mark.parametrize('file_path', articles)
+def test(file_path):
     expected_sentiment, article_text = read_file(file_path)
     actual_sentiment = analyze_sentiment(article_text)
     assert expected_sentiment == actual_sentiment, f'Expected {expected_sentiment}, but got {actual_sentiment}'
-
-
-def test_positive_1():
-    execute_test(positive_1)
-
-
-def test_negative_1():
-    execute_test(negative_1)
-
-
-def test_neutral_2():
-    execute_test(neutral_1)
